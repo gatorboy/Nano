402572aef96e40a23c89b959cce1e3a62a1a1a97
Satish Menedi <smenedi@salesforce.com>
2015-09-07 19:08:04 -0700

    Added lint, check style, pmd and findbugs.

--- app/src/main/java/com/smenedi/nano/MainActivity.java
+++ app/src/main/java/com/smenedi/nano/MainActivity.java
@@ -3,45 +3,52 @@ package com.smenedi.nano;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
 
-    //All main app launcher buttons
-    private Button mSpotifyStreamer;
-    private Button mScoresApp;
-    private Button mLibraryApp;
-    private Button mBuildItBigger;
-    private Button mXyzReader;
-    private Button mCapstone;
+    private static final int UNKNOWN = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mSpotifyStreamer = (Button) findViewById(R.id.spotify_streamer);
-        mScoresApp = (Button) findViewById(R.id.scores_app);
-        mLibraryApp = (Button) findViewById(R.id.library_app);
-        mBuildItBigger = (Button) findViewById(R.id.build_it_bigger);
-        mXyzReader = (Button) findViewById(R.id.xyz_reader);
-        mCapstone = (Button) findViewById(R.id.capstone);
-
-        mSpotifyStreamer.setOnClickListener(this);
-        mScoresApp.setOnClickListener(this);
-        mLibraryApp.setOnClickListener(this);
-        mBuildItBigger.setOnClickListener(this);
-        mXyzReader.setOnClickListener(this);
-        mCapstone.setOnClickListener(this);
     }
 
-
-    @Override
-    public void onClick(View v) {
-        showToast(v.getId());
+    /**
+     * Performs click action on the button clicked
+     *
+     * @param v
+     *         View object of the button clicked
+     */
+    @SuppressWarnings("unused") // This is actually used via button click
+    public void appOpen(View v) {
+        final int stringId;
+        switch (v.getId()) {
+        case R.id.spotify_streamer:
+            stringId = R.string.spotify_streamer;
+            break;
+        case R.id.scores_app:
+            stringId = R.string.scores_app;
+            break;
+        case R.id.library_app:
+            stringId = R.string.library_app;
+            break;
+        case R.id.build_it_bigger:
+            stringId = R.string.build_it_bigger;
+            break;
+        case R.id.xyz_reader:
+            stringId = R.string.xyz_reader;
+            break;
+        case R.id.capstone:
+            stringId = R.string.capstone;
+            break;
+        default:
+            stringId = UNKNOWN;
+            break;
+        }
+        showToast(stringId);
     }
 
     /**
